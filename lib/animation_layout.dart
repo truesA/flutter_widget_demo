6143758d9cfc9cbe8936cc5624b6b344f9499bba
lhm <3186834196@qq.com>
2019-01-31 18:47:43 +0800

    新增 动画widget demo  补充

--- lib/animation_layout.dart
+++ lib/animation_layout.dart
@@ -29,6 +29,51 @@ class AnimationDemo extends StatelessWidget {
                       MaterialPageRoute(
                           builder: (context) => ScaleAnimationRoute()));
                 }),
+            OutlineButton(
+                child: Text("自定义路由切换动画"),
+                onPressed: () {
+//                  Navigator.push(context,
+//                      MaterialPageRoute(builder: (context) => PageRouteDemo()));
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          transitionDuration: const Duration(milliseconds: 500),
+                          pageBuilder: (BuildContext context,
+                              Animation<double> animation,
+                              Animation<double> secondaryAnimation) {
+                            return FadeTransition(
+                                //使用渐隐渐入过渡
+                                child: PageRouteDemo(),
+                                opacity: animation);
+                          }));
+                }),
+            Hero(
+                tag: "outline",
+                child: OutlineButton(
+                    child: Text("Hero动画 共享元素转换"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              transitionDuration:
+                                  const Duration(milliseconds: 500),
+                              pageBuilder: (BuildContext context,
+                                  Animation<double> animation,
+                                  Animation<double> secondaryAnimation) {
+                                return FadeTransition(
+                                    //使用渐隐渐入过渡
+                                    child: HeroRouteDemo(),
+                                    opacity: animation);
+                              }));
+                    })),
+            OutlineButton(
+                child: Text("交错动画 组合动画"),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => StaggerAnimationDemo()));
+                }),
           ],
         ),
       ),
@@ -305,7 +350,7 @@ class _AnimatedBuilderDemoState extends State<AnimatedBuilderDemo>
   Animation<double> animation;
   AnimationController controller;
 
-  @override  
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
@@ -404,5 +449,235 @@ class GrowTransition extends StatelessWidget {
           },
           child: child),
     );
+  }
+}
+
+// ignore: slash_for_doc_comments
+/**
+ * 如果想自定义路由切换动画，可以使用PageRouteBuilder，例如我们想以渐隐渐入动画来实现路由过渡
+ */
+class PageRouteDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("渐隐渐入动画进入"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: OutlineButton(
+                child: Text("自定义跳转FadeRoute"),
+                onPressed: () {
+                  Navigator.push(context,
+                      FadeRoute(builder: (context) => FadeRouteDemo()));
+                }),
+          ),
+        ));
+  }
+}
+
+class FadeRouteDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("自定义跳转FadeRoute"),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class FadeRoute extends PageRoute {
+  FadeRoute({
+    @required this.builder,
+    this.transitionDuration = const Duration(milliseconds: 300),
+    this.opaque = true,
+    this.barrierDismissible = false,
+    this.barrierColor,
+    this.barrierLabel,
+    this.maintainState = true,
+  });
+
+  final WidgetBuilder builder;
+
+  @override
+  final Duration transitionDuration;
+
+  @override
+  final bool opaque;
+
+  @override
+  final bool barrierDismissible;
+
+  @override
+  final Color barrierColor;
+
+  @override
+  final String barrierLabel;
+
+  @override
+  final bool maintainState;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+          Animation<double> secondaryAnimation) =>
+      builder(context);
+
+//  @override
+//  Widget buildTransitions(BuildContext context, Animation<double> animation,
+//      Animation<double> secondaryAnimation, Widget child) {
+//    return FadeTransition(
+//      opacity: animation,
+//      child: builder(context),
+//    );
+//  }
+
+  // 假如我们只想在打开新路由时应用动画，而在返回时不使用动画，那么我们在构建过渡动画时就必须判断当前路由isActive属性是否为true，
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    // TODO: implement buildTransitions
+    //当前路由被激活，是打开新路由
+    if (isActive) {
+      return FadeTransition(
+        opacity: animation,
+        child: builder(context),
+      );
+    } else {
+      //是返回，则不应用过渡动画
+      return Padding(padding: EdgeInsets.zero);
+    }
+  }
+}
+
+class HeroRouteDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HeroRouteDemo"),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: Hero(
+            tag: "outline",
+            child:
+                OutlineButton(child: Text("HeroRouteDemo"), onPressed: () {})),
+      ),
+    );
+  }
+}
+
+class StaggerAnimationDemo extends StatefulWidget {
+  @override
+  _StaggerAnimationDemoState createState() => new _StaggerAnimationDemoState();
+}
+
+class _StaggerAnimationDemoState extends State<StaggerAnimationDemo>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+  }
+
+  Future<Null> _playAnimation() async {
+    try {
+      //先正向执行动画
+      await _controller.forward().orCancel;
+      //再反向执行动画
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("交错动画 组合动画"),
+          centerTitle: true,
+        ),
+        body: GestureDetector(
+          child: Center(
+            child: Container(
+              width: 300.0,
+              height: 350.0,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              //调用我们定义的交错动画Widget
+              child: StaggerAnimationBuilderDemo(controller: _controller),
+            ),
+          ),
+          behavior: HitTestBehavior.opaque,
+          onTap: _playAnimation,
+        ));
+  }
+}
+
+// ignore: slash_for_doc_comments
+/**
+ *https://book.flutterchina.club/chapter9/stagger_animation.html
+ */
+
+class StaggerAnimationBuilderDemo extends StatelessWidget {
+  StaggerAnimationBuilderDemo({Key key, this.controller}) : super(key: key) {
+    //改变高度
+    height = Tween<double>(
+      begin: .0,
+      end: 300,
+    ).animate(CurvedAnimation(
+        parent: controller, curve: Interval(0, 0.6, curve: Curves.ease)));
+
+//    height = Tween<double>(
+//      begin: 100,
+//      end: 200,
+//    ).animate(CurvedAnimation(
+//        parent: controller, curve: Interval(0.3, 0.6, curve: Curves.ease)));
+
+    //改变颜色
+    color = ColorTween(begin: Colors.white, end: Colors.orange).animate(
+        CurvedAnimation(
+            parent: controller, curve: Interval(0, 0.6, curve: Curves.ease)));
+
+    padding = Tween<EdgeInsets>(
+            begin: EdgeInsets.only(left: .0), end: EdgeInsets.only(left: 100.0))
+        .animate(CurvedAnimation(
+            parent: controller, curve: Interval(0.6, 1.0, curve: Curves.ease)));
+  }
+
+  final Animation<double> controller;
+  Animation<double> height;
+  Animation<EdgeInsets> padding;
+  Animation<Color> color;
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: padding.value,
+      child: Container(
+        color: color.value,
+        width: 50.0,
+        height: height.value,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new AnimatedBuilder(animation: controller, builder: _buildAnimation);
   }
 }
